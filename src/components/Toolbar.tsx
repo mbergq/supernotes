419d56f9701d97f6676b48eb0f68f2a18717ae2b
Martin <106612709+menkodot@users.noreply.github.com>
2024-07-12 15:21:26 +0200

    test concept of adding new note

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,14 +1,27 @@
 import { ToolbarContainer } from "../styled-components/toolbar/ToolbarContainer";
 import { AddButton } from "../styled-components/toolbar/S.Button";
+import { useState } from "react";
 
+interface NoteValues {
+  id: number;
+  content: string;
+}
+interface Values extends Array<NoteValues> {}
+
+let nextId = 0;
 function Toolbar() {
+  const [note, setNote] = useState<Values>([]);
   function handleClick() {
+    setNote([...note, { id: nextId++, content: "Hi" }]);
     console.log("Clicked");
   }
   return (
     <>
       <ToolbarContainer>
         <AddButton onClick={handleClick}>new</AddButton>
+        {note.map((n) => (
+          <p key={n.id}>{n.id}</p>
+        ))}
       </ToolbarContainer>
     </>
   );
