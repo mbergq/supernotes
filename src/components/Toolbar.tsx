74578c58129aa69ea188a75d902a23d2b1e2b16d
Martin <106612709+menkodot@users.noreply.github.com>
2024-07-24 11:49:46 +0200

    add input field and radio buttons foundation

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -3,10 +3,33 @@ import { ToolbarContainer } from "../styled-components/toolbar/ToolbarContainer"
 import { AddButton } from "../styled-components/toolbar/S.Button";
 import { useState } from "react";
 
+import { Formik, Form, Field, useField } from "formik";
+import * as Yup from "yup";
+
 interface NoteValues {
   id: number;
   content: string;
 }
+
+const TextInput = ({
+  label,
+  ...props
+}: {
+  [x: string]: string;
+  name: string;
+}) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
 // interface Values extends Array<NoteValues> {}
 
 let nextId = 0;
@@ -24,6 +47,49 @@ function Toolbar() {
         {note.map((n) => (
           <p key={n.id}>{n.id}</p>
         ))}
+        <Formik
+          initialValues={{
+            content: "",
+            color: "",
+          }}
+          validationSchema={Yup.object({
+            content: Yup.string(),
+            //add conditions if needed later here
+            // .max(15, "Must be 15 characters or less")
+            // .required("Required"),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }}
+        >
+          {({ values }) => (
+            <Form>
+              <TextInput label="" name="content" type="text" />
+              <div id="radio-buttons">Color</div>
+              <div role="group" aria-labelledby="radio-buttons">
+                <label>
+                  <Field type="radio" name="color" value="nice-blue" />
+                  nice-blue
+                </label>
+                <label>
+                  <Field type="radio" name="color" value="coral-red" />
+                  coral-red
+                </label>
+                <label>
+                  <Field type="radio" name="color" value="cola" />
+                  cola
+                </label>
+                <label>
+                  <Field type="radio" name="color" value="magenta" />
+                  magenta
+                </label>
+                <div>{values.color}</div>
+                <button type="submit">add</button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </ToolbarContainer>
       <Notes notes={note} />
     </>
