fffec6ad3dc5d81704c61eda191c67a7978e51fc
Martin <106612709+menkodot@users.noreply.github.com>
2024-08-01 12:14:50 +0200

    put prop values into textarea input field to enable live editing for new notes

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,4 +1,6 @@
-import { NoteFrame } from "../styled-components/notes/NoteFrame";
+import { StyledInput } from "../styled-components/toolbar/StyledInput";
+import { Formik, useField } from "formik";
+import * as Yup from "yup";
 
 interface Props {
   data: {
@@ -7,13 +9,48 @@ interface Props {
   };
 }
 
-function Note({ data }: Props) {
+interface TextInputProps {
+  [x: string]: string | object;
+  name: string;
+  style: { backgroundColor: string };
+}
+
+const TextInput = ({ ...props }: TextInputProps) => {
+  const [field, meta] = useField(props);
   return (
     <>
-      <NoteFrame $inputBGColor={data.color}>
-        <p>{data.content}</p>
-      </NoteFrame>
+      <StyledInput className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
     </>
+  );
+};
+
+function Note({ data }: Props) {
+  return (
+    <Formik
+      initialValues={{
+        content: data.content,
+        color: "",
+      }}
+      validationSchema={Yup.object({
+        content: Yup.string(),
+        //add conditions if needed later here
+        // .max(15, "Must be 15 characters or less")
+        // .required("Required"),
+      })}
+      onSubmit={(_values, { setSubmitting }) => {
+        // setNote([...note, { id: nextId++, values }]);
+        setSubmitting(false);
+      }}
+    >
+      <TextInput
+        name="content"
+        type="text"
+        style={{ backgroundColor: data.color }}
+      ></TextInput>
+    </Formik>
   );
 }
 
--- src/styled-components/toolbar/StyledInput.tsx
+++ src/styled-components/toolbar/StyledInput.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const StyledInput = styled.textarea`
-  width: 100%;
-  height: 224px;
+  width: 252px;
+  height: 272px;
   /* padding: 12px 20px; */
   margin-top: 20px;
   margin-bottom: 20px;
